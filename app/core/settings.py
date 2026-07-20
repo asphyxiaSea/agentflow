7800743b9e8d0df3445e60af16c51cf21e5d4f7e
asphyxiasea <guozhen950716@gmail.com>
2026-07-20 16:32:38 +0800

    rag测试准备，测试书本存入向量数据库

--- app/core/settings.py
+++ app/core/settings.py
@@ -12,14 +12,14 @@ DEFAULT_MODEL_PROVIDER = "ollama"
 DEFAULT_TEMPERATURE = 0.1
 DEFAULT_MAX_TOKENS = 1000
 
-RAG_CHROMA_PERSIST_DIR = os.environ.get("RAG_CHROMA_PERSIST_DIR", "./chroma_db")
-RAG_CHROMA_COLLECTION = os.environ.get("RAG_CHROMA_COLLECTION", "rag_default")
+RAG_CHROMA_PERSIST_DIR = os.environ.get("RAG_CHROMA_PERSIST_DIR", "./.chroma")
+RAG_CHROMA_COLLECTION = os.environ.get("RAG_CHROMA_COLLECTION", "rag_test_collection")
 RAG_EMBEDDING_MODEL = os.environ.get("RAG_EMBEDDING_MODEL", "nomic-embed-text")
 RAG_EMBEDDING_BASE_URL = os.environ.get("RAG_EMBEDDING_BASE_URL", OLLAMA_BASE_URL)
-RAG_CHUNK_SIZE = int(os.environ.get("RAG_CHUNK_SIZE", "800"))
-RAG_CHUNK_OVERLAP = int(os.environ.get("RAG_CHUNK_OVERLAP", "120"))
+RAG_CHUNK_SIZE = int(os.environ.get("RAG_CHUNK_SIZE", "500"))
+RAG_CHUNK_OVERLAP = int(os.environ.get("RAG_CHUNK_OVERLAP", "50"))
 RAG_RETRIEVAL_TOP_K = int(os.environ.get("RAG_RETRIEVAL_TOP_K", "4"))
-RAG_DEFAULT_KNOWLEDGE_DOMAIN = os.environ.get("RAG_DEFAULT_KNOWLEDGE_DOMAIN", "agriculture")
+RAG_DEFAULT_KNOWLEDGE_DOMAIN = os.environ.get("RAG_DEFAULT_KNOWLEDGE_DOMAIN", "science_popularization")
 
 TASK_QUEUE_MAXSIZE = int(
     os.environ.get("TASK_QUEUE_MAXSIZE", os.environ.get("RAG_TASK_QUEUE_MAXSIZE", "100"))
